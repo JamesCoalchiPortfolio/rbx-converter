1b96badd8abd8fdd3f86d011fbb1b4aa660d422f
JamesCoalchiPortfolio <seanfaust90@gmail.com>
2025-06-14 20:32:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const robuxInput = document.getElementById('robux');
+    const dollarsInput = document.getElementById('dollars');
 
+    const ROBUX_TO_USD_RATE = 1.00; 
+
+    function formatNumber(num) {
+        if (Number.isInteger(num)) {
+            return num.toString();
+        }
+        return num.toFixed(2);
+    }
+
+    robuxInput.addEventListener('input', () => {
+        const robuxValue = parseFloat(robuxInput.value);
+        if (!isNaN(robuxValue) && robuxValue >= 0) {
+            dollarsInput.value = formatNumber(robuxValue * ROBUX_TO_USD_RATE);
+        } else if (robuxInput.value === '') {
+            dollarsInput.value = '';
+        }
+    });
+
+    dollarsInput.addEventListener('input', () => {
+        const dollarsValue = parseFloat(dollarsInput.value);
+        if (!isNaN(dollarsValue) && dollarsValue >= 0) {
+            robuxInput.value = formatNumber(dollarsValue / ROBUX_TO_USD_RATE);
+        } else if (dollarsInput.value === '') {
+            robuxInput.value = '';
+        }
+    });
+});
